bc7e88a1def15a6b053d599e7bfa9e680ce1bdf0
Batuhan Olcay <bolcay@2-D-1.42heilbronn.de>
2025-10-28 12:50:06 +0100

    just updatn

---
+++ ex02/Account.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bolcay <bolcay@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 12:27:39 by bolcay            #+#    #+#             */
+/*   Updated: 2025/10/28 12:46:22 by bolcay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+void    Account::makeDeposit(int deposit)
+{
+}
+
+bool    Account::makeWithdrawal(int withdrawal)
+{
+    return (true);
+}
+
+int Account::checkAmount(void) const
+{
+}
+
+void    Account::displayStatus(void) const
+{
+}
+
+static void Account::displayTimestamp(void)
+{
+}
